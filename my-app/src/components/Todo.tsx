041435c2da91a8d6d76a603c4f76d8b849702aae
eva <>
2022-09-19 23:56:29 +0800

    优化

--- my-app/src/components/Todo.tsx
+++ my-app/src/components/Todo.tsx
@@ -1,32 +1,14 @@
 import React, { useState, useEffect, ChangeEvent } from 'react'
-import TodoItem from './TodoItem'
+
+import useTodoState from '../hooks/useTodoState'
 
 
 const Todo: React.FC = () => {
-  const [items, setItems ] = useState([{ message: 'ww' }])
-  const [input, setInput] = useState<any>("")
-  const addItem = () => {
-    const item2 = [...items, { message: input }]
-    setItems(item2)
-    setInput("")
-  }
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value)
-  }
-  const handleDelete = (index: number) => {
-    const l2 = [...items]
-    l2.splice(index, 1)
-    setItems(l2)
-  }
-  const getTodoItem = () => {
-    return items.map((item, index) => {
-        return (<TodoItem key={index} index={ index } message={ item.message } itemDelete={ handleDelete }/>)
-      })
-  }
+  const [items, input, addItem, handleOnChange, handleDelete, getTodoItem] = useTodoState()
   return (
     <ul >
       <input onChange={ handleOnChange } value={ input }/>
-      { getTodoItem() }
+        { getTodoItem() }
       <button onClick={ addItem }>addItem</button>
     </ul>)
 }
---
+++ my-app/src/hooks/useTodoState.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import TodoItem from '../components/TodoItem'
+
+
+const useTodoState = () => {
+  const [items, setItems ] = useState([
+      { message: 'ww' }
+    ])
+  const [input, setInput] = useState<any>( "" )
+  const addItem = () => {
+    const item2 = [...items, { message: input }]
+    setItems(item2)
+    setInput("")
+  }
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+  const handleDelete = (index: number) => {
+    const l2 = [...items]
+    l2.splice(index, 1)
+    setItems(l2)
+  }
+  const getTodoItem = () => {
+    return items.map((item, index) => {
+        return (<TodoItem key={index} index={ index } message={ item.message } itemDelete={ handleDelete }/>)
+      })
+  }
+  return [items, input, addItem, handleOnChange, handleDelete, getTodoItem]
+}
+
+export default useTodoState
