4fbf747a1a6ae132fb9f4ea91dc35956103346fc
eva <>
2022-09-19 23:17:12 +0800

    优化

--- my-app/src/hooks/useMousePosition.tsx
+++ my-app/src/hooks/useMousePosition.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 
 const useMousePosition = () => {
-  const [ positions, setPositions] = useState({x: 0, y: 0})
+  const [ positions, setPositions ] = useState({ x: 0, y: 0 })
   useEffect(() => {
     console.log('add effect', positions.x)
     const updateMouse = (e: MouseEvent) => {
       console.log("inner")
-      setPositions({x: e.clientX, y: e.clientY })
+      setPositions({ x: e.clientX, y: e.clientY })
     }
     document.addEventListener('mousemove', updateMouse)
     return () => {
