98db26cfda14f3b5dbdaf4c2fe41d9a1d4d57453
eva <>
2022-10-16 16:28:40 +0800

    new

--- lib-react/src/App.tsx
+++ lib-react/src/App.tsx
@@ -2,18 +2,30 @@ import React from 'react';
 import Button, { ButtonType, ButtonSize} from "./components/Button/button"
 import Menu from "./components/Menu/menu"
 import MenuItem from "./components/Menu/menuItem"
+import SubMenu from './components/Menu/subMenu'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={0} >
+        <Menu defaultIndex={'0'} mode="vertical" defaultOpenSubMenus={['2']}>
           <MenuItem>
             cool link
           </MenuItem>
           <MenuItem disabled> 
             cool link 2
           </MenuItem>
+          <SubMenu title="dropdown">
+            <MenuItem>
+              dropdown1
+            </MenuItem>
+            <MenuItem>
+              dropdown2
+            </MenuItem>
+            <MenuItem>
+              dropdown3
+            </MenuItem>
+          </SubMenu>
           <MenuItem>
             cool link 3
           </MenuItem>
