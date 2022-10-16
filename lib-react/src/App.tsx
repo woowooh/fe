c43a79032940d927cf29e3883e88e2cc910f3aa6
eva <>
2022-10-16 14:26:29 +0800

    new

--- lib-react/src/App.tsx
+++ lib-react/src/App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize} from "./components/Button/button"
+import Menu from "./components/Menu/menu"
+import MenuItem from "./components/Menu/menuItem"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Menu defaultIndex={0} >
+          <MenuItem>
+            cool link
+          </MenuItem>
+          <MenuItem disabled> 
+            cool link 2
+          </MenuItem>
+          <MenuItem>
+            cool link 3
+          </MenuItem>
+        </Menu>
+        <Button autoFocus onClick={ (e) => {e.preventDefault(); alert('www')}}>sample </Button>
+        <Button btnType={ButtonType.Default}> Hello button </Button>
+        <Button disabled btnType={ButtonType.Default}> Hello button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large hello </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}> small Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> small danger </Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com" disabled> baidu </Button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
